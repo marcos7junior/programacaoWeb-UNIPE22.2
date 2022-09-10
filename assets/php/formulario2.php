1bb1e3dd71878a98481fa4e2e7b8ce03a4a354e6
marcos7junior <marcospaivajr7@gmail.com>
2022-09-10 16:10:52 -0300

    Adicionado formulario 2

---
+++ assets/php/formulario2.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Formulário</title>
+</head>
+
+<body>
+    <h1>Formulário de Inscrição</h1>
+    <h3>Preencha seus dados</h3>
+
+    <form>
+    
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome"></br>
+        </br>
+        <label for="email">Email</label>
+        <input type="email" name="email"></br>
+        </br>
+        <label for="endereço">Endereço:</label>
+        <input type="text" name="endereço"></br>
+        </br> 
+        <label for="cidade">Cidade:</label>
+        <input type="text" name="cidade"></br>
+        </br>
+        <label for="telefone">Telefone:</label>
+        <input type="tel" name="telefone" size="3">
+        <input type="tel" name="telefone"></br>
+        </br>
+        <label for="usuário">Usuário:</label>
+        <input type="text" name="usuário" placeholder="Entre com o seu login"></br>
+        </br>
+        <label for="senha">Senha:</label>
+        <input type="password" name="senha"></br>
+        </br>
+        <input type="submit" value="Finalizar minha inscrição">
+        
+    </form>
+
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="assets/css/style.css">
-  	<title>Atividade 5 - Formulários</title>
+	<link rel="stylesheet" href="./assets/css/style.css">
+  <title>Atividade 5 - Formulários</title>
 </head>
 <body>
 	<h1 class="titulo_index">Atividade 5 - Formulários</h1>
@@ -15,7 +15,7 @@
 			<p class="titulo">Formulário 1</p>
 		</div>
 		<div class="card">
-			<a href=""><img src="./assets/img/form.png" alt=""></a>
+			<a href="./assets/php/formulario2.php" target="_blank"><img src="./assets/img/form.png" alt=""></a>
 			<p class="titulo">Formulário 2</p>
 		</div>
 		<div class="card">
@@ -31,6 +31,14 @@
 			<p class="titulo">Formulário 5</p>
 		</div>
 	</div>
+
+	<section class="grupo">
+			<h1>Grupo:</h1>
+			<p>Ana vitória</p>
+			<p>Marcos Antônio</p>
+
+	</section>
+
 </body>
 </html>
 
